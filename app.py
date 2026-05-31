e5f111276a5143fcb9db6c5cb60ec8becd164f26
AnkitShakya98 <vimla3119@gmail.com>
2026-05-31 11:57:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,18 @@ from pyrogram import idle
 from Extractor import app
 from Extractor.modules import ALL_MODULES
 from myutils.cleanup import start_cleanup_scheduler
+from pyrogram import utils
 
+def get_peer_type_new(peer_id: int) -> str:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
+        return "user"
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
+
+utils.get_peer_type = get_peer_type_new
 # Flask app
 flask_app = Flask(__name__)
 
